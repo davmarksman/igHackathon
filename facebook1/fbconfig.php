ccee68f206287312d8098dc65d284a6a03860059
Justin <ju3tin@hotmail.co.uk>
2016-09-18 03:18:02 +0100

    update

--- facebook1/fbconfig.php
+++ facebook1/fbconfig.php
@@ -14,7 +14,8 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 // init app with app id and secret
-FacebookSession::setDefaultApplication( 'Your APP ID','Your APP Secret' );
+FacebookSession::setDefaultApplication( '301809796851119','
+a974367187e255a3714f638ed377f158' );
 // login helper with redirect_uri
     $helper = new FacebookRedirectLoginHelper('http://demos.krizna.com/1353/fbconfig.php' );
 try {
